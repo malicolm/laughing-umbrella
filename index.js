b207339fc50bd7492768844bc8adc605bf873423
Malcolm <holmanm@oxy.edu>
2021-11-01 16:15:26 -0700

    something

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// import axios from "axios"
+import axios from "axios"
 
-// const handleGetRandomUser =  async () => {
-//     const response = await axios.get('https://randomuser.me/api/')
-//     const username = response.data.login.username
-//     const generatedUser = {
-//         gender: response.data.gender,
-//         firstName: response.data.name.first,
-//         lastName: response.data.name.last,
-//         email: response.data.email,
-//         age: response.data.dob.age,
-//     }
-//     //can append text from here
-//     //using create eleent
-//     return generatedUser
-// }
+const handleGetRandomUser =  async () => {
+    const response = await axios.get('https://randomuser.me/api/')
+    const username = response.data.login.username
+    const generatedUser = {
+        gender: response.data.gender,
+        firstName: response.data.name.first,
+        lastName: response.data.name.last,
+        email: response.data.email,
+        age: response.data.dob.age,
+    }
+    //can append text from here
+    //using create eleent
+    return generatedUser
+}
 
 export function populateText(){
     let ele = document.getElementById('container');
